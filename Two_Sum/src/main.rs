4b66997507b6d280c1c840bdf44b7cae67ad0f64
Kevin Yang <ky200617@gmail.com>
2021-08-27 13:51:57 -0400

    working on solution

--- Two_Sum/src/main.rs
+++ Two_Sum/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut sorted = nums;
+        sorted.sort(); 
+
+        let sorted_len = sorted.len().into();
+        // Impl. of binary search
+        let mut middle: i32 = sorted_len / 2; 
+        let mut lower: i32 = 0;
+        let mut upper: i32 = sorted_len; 
+        for num in sorted{
+            println!("{}", num);
+
+        }
+     return [1,2].to_vec()
+    }
+
+    let arr = vec![32, 2, 4, 10, 28, 30];    
+
+    two_sum(arr, 32);
+    
 }
